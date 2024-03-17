67b021dee5894844dc3273389dca24f2186baf99
Gustavo Paulo <gustavo.p07@aluno.ifsc.edu.br>
2024-03-17 12:44:23 -0300

    update inc

--- paq.py
+++ paq.py
@@ -6,7 +6,7 @@ def encontraPaq(dado, paq):
     
     # Procura o PAQ
     if dado[A : A+1] == '1' and dado[fim : fim + 8] == paq:
-        #print(dado[0:fim], end='')
+        print(dado[0:fim], end='')
         # Adiciona na lista 'timeSlot', de 8 em 8 bits
         timeSlot = [dado[j : j+8] for j in range(0, fim, 8)]    
     return timeSlot
@@ -32,3 +32,5 @@ with open('RX(vetor)MQ_v2.txt') as arq:
         else:
             timeSlot = encontraPaq(dado[start:], paq)
             print(timeSlot)
+            print(start)
+            start += 1
