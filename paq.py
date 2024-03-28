b0d601a7999a3da7bfda083bef8ac6c38213dd72
gustavo.p07 <gustavo.p07@aluno.ifsc.edu.br>
2024-03-28 09:57:33 -0300

    :sparkles: feat: finalizado

--- paq.py
+++ paq.py
@@ -18,14 +18,13 @@ with open('RX(vetor)MQ_v2.txt') as arq:
     dado = dado.replace(' ', '')
     start = 0
 
-    quadro = []
     PAQ = '10011011'
+    quadro = 0
     
     while True:
         # Encontra o PAQ
         
         start = dado.find(PAQ, start)
-        
         # Ao terminar o arquivo, sai do loop
         if start == -1:
             break
@@ -34,8 +33,18 @@ with open('RX(vetor)MQ_v2.txt') as arq:
             timeslot = organiza_timeslots(dado[start:], PAQ)        
             if len(timeslot) > 0:
                 PAQ = (timeslot.pop(0))
+                quadro += 1
+                print(f'Quadro: {quadro}')
+                print(f'timeslot 0 (paq): {PAQ}, encontrado na posição {start}')
                 for idx, i in enumerate(timeslot):
-                    print(f'timeslot {idx}: {i}')
-                print(f'timeslot 63 (paq): {PAQ}, encontrado na posição {start}')
+                    if idx > 30:
+                        if idx == 31:
+                            quadro += 1
+                            print(f'Quadro: {quadro}')
+                        ts = idx - 32
+                    else:
+                        ts = idx
+                    print(f'timeslot {ts + 1}: {i}')
+                
             start += 1
 
