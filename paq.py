60075d047b8bc21d0cd55e2423cf4f29c42cf6fc
Gustavo Paulo <gustavo.p07@aluno.ifsc.edu.br>
2024-04-15 08:53:22 -0300

    bits alinhados ajeitadinho

--- paq.py
+++ paq.py
@@ -13,6 +13,21 @@ def organiza_timeslots(dado, paq):
         timeslot = [dado[j : j+8] for j in range(0, second, 8)]    
     return timeslot
 
+def encontraPamq(pamq, quadro):
+    multiQuadro = []
+
+    for q in quadro:            
+        # verifica o timeslot 16
+        if q[16][:3] == pamq:  
+            if not multiQuadro:
+                multiQuadro.append(q)
+            else:
+                break    
+        elif multiQuadro:
+            multiQuadro.append(q)
+
+    return multiQuadro
+
 with open('RX(vetor)MQ_v2.txt') as arq:
     dado = arq.read()
     dado = dado.replace(' ', '')
@@ -20,6 +35,7 @@ with open('RX(vetor)MQ_v2.txt') as arq:
 
     bits_alinhados = []
     PAQ = '10011011'
+    bits_alinhados.append(PAQ)
     
     while True:
         # Encontra o PAQ
@@ -31,8 +47,8 @@ with open('RX(vetor)MQ_v2.txt') as arq:
             break
         # Enquanto não terminar o arquivo
         else:
-            timeslot = organiza_timeslots(dado[start:], PAQ)        
-            bits_alinhados.append(timeslot)
+            timeslot = organiza_timeslots(dado[start:], PAQ)  
+            bits_alinhados.extend(timeslot)      
             if len(timeslot) > 0:
                 PAQ = (timeslot.pop(0))
                 for idx, i in enumerate(timeslot):
@@ -40,5 +56,12 @@ with open('RX(vetor)MQ_v2.txt') as arq:
                 print(f'timeslot 63 (paq): {PAQ}, encontrado na posição {start}')
             start += 1
 
-
-    print(bits_alinhados)
+    quadro_alinhado = ''.join(map(str, bits_alinhados))
+    print(quadro_alinhado)
+    
+    
+    
+    
+    
+    
+    
