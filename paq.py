523be547a707013d01998de50ee750ae222b27ce
Gustavo Paulo <gustavo.p07@aluno.ifsc.edu.br>
2024-04-16 04:12:34 -0300

    :sparkles: feat: encontrando pamq e extraindo bits de sinalização

--- paq.py
+++ paq.py
@@ -13,54 +13,45 @@ def organiza_timeslots(dado, paq):
         timeslot = [dado[j : j+8] for j in range(0, second, 8)]    
     return timeslot
 
-def verifica_pamq(quadro, pamq):
-    multiQuadro = ''  # Inicializa o multiquadro como uma string vazia
-
-    # Itera sobre os índices do quadro com passos de 8 bits (tamanho de um timeslot)
-    for i in range(0, len(quadro), 8):
-        timeslot = quadro[i:i+8]  # Obtém o timeslot atual de 8 bits
-
-        if timeslot[:3] == pamq:   # Verifica se o time slot 16 possui o PAMQ
-            if not multiQuadro: # Se ainda não houver um multiquadro sendo formado, o inicia
-                multiQuadro += timeslot
-            else:
-                break    
-        elif multiQuadro:
-            multiQuadro += timeslot
-        
-
-    return multiQuadro
-
-
 with open('RX(vetor)MQ_v2.txt') as arq:
     dado = arq.read()
     dado = dado.replace(' ', '')
     start = 0
 
-    bits_alinhados = []
     PAQ = '10011011'
-    bits_alinhados.append(PAQ)
+    quadro = 0
+    pamq_encontrado = False
     
     while True:
         # Encontra o PAQ
         
         start = dado.find(PAQ, start)
-        
         # Ao terminar o arquivo, sai do loop
         if start == -1:
             break
         # Enquanto não terminar o arquivo
         else:
-            timeslot = organiza_timeslots(dado[start:], PAQ)  
-            bits_alinhados.extend(timeslot)      
+            timeslot = organiza_timeslots(dado[start:], PAQ)        
             if len(timeslot) > 0:
                 PAQ = (timeslot.pop(0))
+                quadro += 1
+                print(f'Quadro: {quadro}')
+                print(f'timeslot 0 (paq): {PAQ}, encontrado na posição {start}')
                 for idx, i in enumerate(timeslot):
-                    print(f'timeslot {idx}: {i}')
-                    #TODO verificar se é o timeslot 16 e aí tirar os bits de sinalização
-                print(f'timeslot 63 (paq): {PAQ}, encontrado na posição {start}')
+                    if idx > 30:
+                        if idx == 31:
+                            quadro += 1
+                            print(f'Quadro: {quadro}')
+                        ts = idx - 32
+                    else:
+                        ts = idx
+                    print(f'timeslot {ts + 1}: {i}')
+                    if ts + 1 == 16:
+                        if pamq_encontrado:
+                            print(f'Canal {quadro}: {i[0]}{i[1]}')
+                            print(f'Canal {quadro+15}: {i[4]}{i[5]}')
+                        else:
+                            pamq_encontrado = True
+                            print('PAMQ encontrado')
                 
             start += 1
-
-    quadro_alinhado = ''.join(map(str, bits_alinhados))
-    #print(quadro_alinhado)
